8eea9c382d8688de89409a49cd883635cf4e9fb7
corbineau <lochinvar@gmail.com>
2019-07-04 16:49:04 -0400

    base structure & dependencies

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// Serve the static files from the React app
+app.use(express.static(path.join(__dirname, 'rabbitworks/build')));
+
+
+
+const port = process.env.PORT || 5000;
