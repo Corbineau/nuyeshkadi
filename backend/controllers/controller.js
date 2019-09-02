2aec325e76ca6c445331a527ed6523cccdc7b401
corbineau <lochinvar@gmail.com>
2019-09-01 20:44:19 -0400

    futzing about with the controllers

--- backend/controllers/controller.js
+++ backend/controllers/controller.js
@@ -3,8 +3,9 @@ const db = require("../models")
 // Defining methods for the booksController
 module.exports = {
     findAll: function(req, res) {
+      console.log("searching for all words...");
       db.Yesh
-        .find(req.query)
+        .find({})
         .sort({ word: 1 })
         .then(dbModel => res.json(dbModel))
         .catch(err => res.status(422).json(err));
