2ca13b542907c37228935cadaa54100edade4aa3
Rabbit <Corbineau@users.noreply.github.com>
2019-09-18 17:36:27 -0400

    tweaking syntax

--- backend/controllers/controller.js
+++ backend/controllers/controller.js
@@ -1,6 +1,6 @@
 const db = require("../models")
 
-// Defining methods for the booksController
+// Defining methods for the Controller
 module.exports = {
     findAll: function(req, res) {
       db.Yesh
