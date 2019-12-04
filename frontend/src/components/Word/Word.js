5f42bfe73ea6596c49c8216ae3a66ea6d0e9b62c
corbineau <lochinvar@gmail.com>
2019-12-03 22:20:41 -0500

    starting to work on the actual page structure for admin

--- frontend/src/components/Word/Word.js
+++ frontend/src/components/Word/Word.js
@@ -2,18 +2,22 @@ import React from "react";
 import "./Word.css"
 
 //Actually, I think I want the date and the word outside of this component, so that I can reuse it in different contexts? IDK.
+//need to be able to add multiple meanings, each with it's own part of speech, so include all those components here
 
-function Word({ yesh, meaning, pronunciation, partOfSpeech}) {
+function Word({ word, meaning, orthography}) {
     return (
         <div className="word">
             <p>
-                <strong>{yesh}</strong>
+                <strong>{word}</strong>
             </p>
             <div className="types">
                 {pronunciation} {partOfSpeech}
             </div>
             <div className="meaning">
                 {meaning}
+            </div>
+            <div className="orthography">
+                {orthography}
             </div>
         </div>
     );
