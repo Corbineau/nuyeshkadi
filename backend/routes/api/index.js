ad7b5a425aa83fb226fa108b4acb40ec9cb16353
corbineau <lochinvar@gmail.com>
2020-01-08 21:49:22 -0500

    cleanup

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,4 +1,4 @@
-const path = require("path");
+// const path = require("path");
 const router = require("express").Router();
 const tanRoutes = require("./tan");
 const yeshRoutes = require("./yesh");
